3403f3c04437f996898a1444e6b3201682695060
Sharofiddin Rakhimov <114148093+Sharofbek84@users.noreply.github.com>
2026-09-04 22:46:56 +1000

    Barcha sahifalarda yagona footer matni

---
+++ app/components/SiteFooter.tsx
@@ -0,0 +1,7 @@
+export default function SiteFooter() {
+  return (
+    <footer className="footer">
+      GOLDENWEB.UZ © 2026 • Real vaqtda kripto bozor tahlili va kripto yangiliklari
+    </footer>
+  )
+}
