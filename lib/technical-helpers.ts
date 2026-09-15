5e315c85fa22640837621b173c1ec1fe108d58a1
Sharofiddin Rakhimov <114148093+Sharofbek84@users.noreply.github.com>
2026-09-15 21:55:04 +1000

    Add technical-helpers split modules (EMA50 filter support)

---
+++ lib/technical-helpers.ts
@@ -0,0 +1,13 @@
+export type { Candle, Divergence, TechnicalResult } from './technical-helpers-a'
+export {
+  ema,
+  rsi,
+  detectRsiDivergence,
+  tfLabel,
+  fmt,
+} from './technical-helpers-a'
+export {
+  lastSwingLevels,
+  longLevels,
+  shortLevels,
+} from './technical-helpers-b'
